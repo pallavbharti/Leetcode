4deaa1c1edd5c082969aa23ac325f2ed81eb4f23
Pallav Bharti <146830084+pallavbharti@users.noreply.github.com>
2025-11-20 17:44:01 +0530

    Time: 0 ms (100.00%) | Memory: 26.2 MB (48.14%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -11,18 +11,14 @@ public:
             if(max<height[i]) max =  height[i];
         }
         // next greatest element: 
-        int next[n];
-        next[n-1]= -1;
+        
+        prev[n-1]= -1;
         max = height[n-1];
         for(int i= n-2; i>0; i--){
-            next[i]=max;
+            if(max<prev[i]) prev[i]=max;
             if(max<height[i]) max = height[i];
         }
-        // minimum array :
-        
-        for(int i =0 ; i<n ;i++){
-            prev[i] = min(prev[i],next[i]);
-        }
+    
         // calculating water: 
         int water =0;
         for(int i=1; i<n-1; i++){
