ed1cf71630414f4b10e8049e1a5a8ba2cd274407
Pallav Bharti <146830084+pallavbharti@users.noreply.github.com>
2025-11-23 01:07:31 +0530

    Time: 0 ms (100.00%) | Memory: 10 MB (10.95%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,22 +1,19 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        unordered_map<char,int> m1;
-        unordered_map<char,int> m2;
-        if(s.length() != t.length()) return false;
-        
-        for(int i = 0;i<s.length();i++){
-            m1[s[i]]++;
-            m2[t[i]]++;
+        if(s.length() != t.length())return false;
+        unordered_map<char,int>m;
+        for(int i =0 ; i<s.length() ; i++){
+            m[s[i]]++;    //char : freq stored 
         }
-        for(auto x:m1){
-            char ch1 = x.first;
-            int freq1 = x.second;
-            if(m2.find(ch1) != m2.end()){
-                int freq2 = m2[ch1];
-                if(freq2 != freq1) return false;
+        for(int i =0 ; i<t.length() ; i++){
+            int ch= t[i];
+            if(m.find(ch) != m.end()){    //if exists reduce freuency and erase if freq = 0:
+                m[ch]--;
+                if(m[ch] == 0) m.erase(ch);
             }else return false;
         }
-        return true;
+        if(m.size()>0) return false;
+        else return true;
     }
 };
