f1a503003f1f83b00a2829dd72145e90ab64cf20
Pallav Bharti <pallavbharti8795@gmail.com>
2026-01-31 00:07:38 +0530

    Time: 4 ms (29.63%) | Memory: 9.4 MB (18.98%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.cpp
+++ 377-combination-sum-iv/combination-sum-iv.cpp
@@ -2,10 +2,10 @@ class Solution {
 public:
     int f(vector<int>& nums, int target,vector<int>&dp){
         if(target==0)return 1;
+        if(target<0)return 0;
         if(dp[target]!=-1) return dp[target];
         int ans = 0;
         for(int i=0;i<nums.size();i++){
-            if(target-nums[i]<0)continue;
             ans += f(nums,target-nums[i],dp);
         }
         return dp[target]=ans;
