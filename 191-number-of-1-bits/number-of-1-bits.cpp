d823ecea0f1565093c528b46d37310619ee8d0ac
Pallav Bharti <pallavbharti8795@gmail.com>
2026-07-14 09:49:12 +0530

    Time: 0 ms (100.00%) | Memory: 8.2 MB (82.69%) - LeetSync

--- 191-number-of-1-bits/number-of-1-bits.cpp
+++ 191-number-of-1-bits/number-of-1-bits.cpp
@@ -1,7 +1,15 @@
 class Solution {
 public:
+    int helper(int n){
+        int count = 0;
+        while(n>0){
+            count++;
+            n = n & (n-1);
+        }return count;
+    }
     int hammingWeight(int n) {
-        int ans = __builtin_popcount(n);
+        //int ans = __builtin_popcount(n);
+        int ans =  helper(n);
         return ans;
     }
 };
