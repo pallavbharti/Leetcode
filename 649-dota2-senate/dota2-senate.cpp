aa148f414f997ca47e98f577013c3d6748f8009b
Pallav Bharti <pallavbharti8795@gmail.com>
2026-09-23 14:39:10 +0530

    Time: 4 ms (41.06%) | Memory: 11.7 MB (7.70%) - LeetSync

---
+++ 649-dota2-senate/dota2-senate.cpp
@@ -0,0 +1,40 @@
+class Solution {
+public:
+    string predictPartyVictory(string senate) {
+        int n = senate.length();
+        queue<int>q;
+        queue<int>r;
+        queue<int>d;
+        for(int i=0;i<n;i++){
+            q.push(i);
+            if(senate[i]=='R')r.push(i);
+            else d.push(i);
+        }
+        while(q.size()>1){
+            if(senate[q.front()]=='X')q.pop();
+            else if(senate[q.front()]=='R'){
+                if(d.size()==0)return "Radiant";
+                else{
+                    senate[d.front()]='X';
+                    d.pop();
+                    q.push(q.front());
+                    q.pop();
+                    r.push(r.front());
+                    r.pop();
+                }
+            }else{
+                if(r.size()==0)return "Dire";
+                else{
+                    senate[r.front()]='X';
+                    r.pop();
+                    q.push(q.front());
+                    q.pop();
+                    d.push(d.front());
+                    d.pop();
+                }
+            }
+        }
+        if(senate[q.front()]=='R')return "Radiant" ;
+        else return "Dire";
+    }
+};
